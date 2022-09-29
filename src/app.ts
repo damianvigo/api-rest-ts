69cdb38b19535f1643c8a2717ab37b536b99f141
Damian Vigo <damiandanielvigo@gmail.com>
2022-09-29 19:43:34 -0300

    add-routes-dynamics

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import 'dotenv/config';
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import { router } from './routes';
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(morgan('dev'));
 app.use(cors());
+app.use(router);
 
 app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
