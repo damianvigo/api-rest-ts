4d8693d88b811bdc719b7ad3425e277034ae85e5
Damian Vigo <damiandanielvigo@gmail.com>
2022-10-03 19:17:22 -0300

    add-config-db

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,12 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import { router } from './routes';
+import db from './config/mongo';
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(morgan('dev'));
 app.use(cors());
 app.use(router);
-
+db().then(() => console.log('connect to MongoDB'));
 app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
